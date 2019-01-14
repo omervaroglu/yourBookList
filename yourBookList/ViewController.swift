25af48548f66f795c904f5f769f7da2a59a69471
Omer <omer@Omer-MacBook-Pro.local>
2019-01-14 19:54:10 +0300

    create book list

--- yourBookList/ViewController.swift
+++ yourBookList/ViewController.swift
@@ -7,14 +7,126 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    var bookList : [String] = []
+    var reading : [Double] = []
+    var selectedBook = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getData()
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.getData), name: NSNotification.Name("newlist"), object: nil)
+    }
+    
+    @objc func getData() {
+        
+        // table view guncellerken tum verileri kaldirip yeniden yukluyoruz.
+        bookList.removeAll()
+        reading.removeAll()
+        
+        
+        //context ve AppDelegate e ulasmak icin gerekli kisim
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        //burasi verileri cekmek icin gerekli
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "BookProperty")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        // do try ve for ile verileri kontrol edip aliyoruz.
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let name = result.value(forKey: "bookName") as? String {
+                        self.bookList.append(name)
+                    }
+                    if let reading1 = result.value(forKey: "reading") as? Double {
+                        self.reading.append(reading1)
+                    }
+                    
+                    self.tableView.reloadData()
+                }
+            }
+        } catch {
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bookList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        // tek bir hucreyi 2 satir olarak kullanmak icin
+        cell.textLabel!.numberOfLines = 2
+        cell.textLabel?.text = "\(bookList[indexPath.row])\(String("\n%\(reading[indexPath.row]) okundu.")) "
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "BookProperty")
+            do{
+                let results = try context.fetch(fetchRequest)
+                
+                for result in results as! [NSManagedObject] {
+                    if let bookName = result.value(forKey: "bookName") as? String {
+                        if bookName == bookList[indexPath.row] {
+                            context.delete(result)
+                            bookList.remove(at: indexPath.row)
+                            reading.remove(at: indexPath.row)
+                            
+                            self.tableView.reloadData()
+                            
+                            do{
+                                try context.save()
+                            }catch {
+                                
+                            }
+                            break
+                        }
+                    }
+                }
+            } catch{
+                
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as? detailsVC
+            destinationVC?.chosenBook = selectedBook
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedBook = bookList[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    @IBAction func addButton(_ sender: Any) {
+        selectedBook = ""
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
 }
 
---
+++ yourBookList/detailsVC.swift
@@ -0,0 +1,105 @@
+//
+//  detailsVC.swift
+//  yourBookList
+//
+//  Created by Omer  on 10.01.2019.
+//  Copyright © 2019 Omer Varoglu. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class detailsVC: UIViewController {
+    @IBOutlet weak var booksName: UITextField!
+    @IBOutlet weak var authorName: UITextField!
+    @IBOutlet weak var willReadPage: UITextField!
+    @IBOutlet weak var readPage: UITextField!
+    
+    var chosenBook = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if chosenBook != ""  {
+            getProperty()
+        }
+
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+       let appDelegate = UIApplication.shared.delegate as! AppDelegate
+       let context = appDelegate.persistentContainer.viewContext
+        
+       let newBook = NSEntityDescription.insertNewObject(forEntityName: "BookProperty", into: context)
+        
+        newBook.setValue(authorName.text, forKey: "authorName")
+        newBook.setValue(booksName.text, forKey: "bookName")
+        
+        if let willReadPage = Double(willReadPage.text!) {
+            newBook.setValue(willReadPage, forKey: "willReadPage")
+        }
+        
+        if let readPage = Double(readPage.text!) {
+            newBook.setValue(readPage, forKey: "readPage")
+        }
+        
+        
+        if let readpage1 = Double(readPage.text!) {
+            if let willReadPage1 = Double(willReadPage.text!) {
+                let x = (readpage1 * 100.0 ) / (willReadPage1)
+                let reading = x.rounded()//en yakin tam sayiya yuvarlama icin kullaniliyor.
+                newBook.setValue(reading, forKey: "reading")
+            }
+        }
+        
+        do {
+            try context.save()
+            print("Basarili")
+        } catch {
+            print(error)
+        }
+        
+        NotificationCenter.default.post(name: NSNotification.Name("newlist"), object: nil)
+        self.navigationController?.popViewController(animated: true)
+        
+    }
+    
+    func getProperty () {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "BookProperty")
+        
+        // burada kucuk bir filtreleme yapiyoruz.
+        fetchRequest.predicate = NSPredicate(format: "bookName = %@", chosenBook)
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    booksName.text = chosenBook
+
+                    if let author = result.value(forKey: "authorName") as? String {
+                        authorName.text = author
+                    }
+                    if let page = result.value(forKey: "willReadPage") as? Double {
+                        willReadPage.text = String(page)
+                    }
+                    if let readingPage = result.value(forKey: "readPage") as? Double {
+                        readPage.text = String(readingPage)
+                    }
+                }
+            }
+        } catch {
+
+        }
+        
+        
+    }
+    
+}
